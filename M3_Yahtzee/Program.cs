e0c60670bd4eb4984a135cf701f27fd351756f83
Martin Lenart <martin_lenart@icloud.com>
2021-10-25 09:48:03 +0200

    Add project files.

---
+++ M3_Yahtzee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M3_Yahtzee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
